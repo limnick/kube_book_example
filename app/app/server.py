d585384ba3e24f27d850bc748180832b8a98f18e
Nick Johnson <njohnson@limcollective.com>
2018-05-14 14:55:35 -0700

    reverting accidental changes to master branch

--- app/app/server.py
+++ app/app/server.py
@@ -13,7 +13,6 @@ print("connected to gcloud bucket")
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
-    self.application.request_count += 1
     blob_iterator = bucket.list_blobs(max_results=10)
     for blob in blob_iterator:
       self.write("{} {}<br>".format(blob.size, blob.name))
@@ -23,23 +22,11 @@ class HealthHandler(tornado.web.RequestHandler):
     self.write("OK")
     self.set_header('Content-Type', 'text/plain')
 
-class MetricsHandler(tornado.web.RequestHandler):
-  def get(self):
-    out_txt = ""
-    metric_name = "WithKubeToyServer_requests_total"
-    out_txt = out_txt + "# HELP {} Total number of requests".format(metric_name) + "\n"
-    out_txt = out_txt + "# TYPE {} counter".format(metric_name) + "\n"
-    out_txt = out_txt + "{} {}".format(metric_name, self.application.request_count) + "\n"
-    self.write(out_txt)
-    self.set_header('Content-Type', 'text/plain')
-
 def make_app():
   tornado_app = tornado.web.Application([
     (r"/", MainHandler),
     (r"/health", HealthHandler),
-    (r"/metrics", MetricsHandler),
   ])
-  tornado_app.request_count = 0
   return tornado_app
 
 
